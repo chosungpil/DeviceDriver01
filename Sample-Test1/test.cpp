1bea6a825f14eb88c88a9daecd340709f03359a5
Sungpil.cho <chosungpil@gmail.com>
2023-06-05 14:00:45 +0900

    Setup

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <iostream>
+#include <string>
+#include "../Project9/DeviceDriver.h"
+using namespace testing;
+using namespace std;
+
+TEST(TestCaseName, DeviceDriverTest) {
+	EXPECT_EQ(1, 1);
+}
