8f28a8c3ae128b5c093b956477dc8551a26ea5fe
Sungpil.cho <chosungpil@gmail.com>
2023-06-05 16:04:10 +0900

    Fix read sleep error

--- Project9/DeviceDriver.cpp
+++ Project9/DeviceDriver.cpp
@@ -9,11 +9,9 @@ int DeviceDriver::read(long address)
     int firstReadValue = (int)m_hardware->read(address);
     for (int readCount = 1; readCount < READ_COUNT ; readCount++)
     {
+        Sleep(READ_DELAY);
         int nextReadValue = m_hardware->read(address);
-        if (firstReadValue == nextReadValue)
-        {
-            Sleep(READ_DELAY);
-        }else
+        if (firstReadValue != nextReadValue)
         {
             throw std::exception("Exception!!");
         }
